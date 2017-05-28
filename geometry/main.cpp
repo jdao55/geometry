4f634e640d311967c65d314ab3f160cf2c2ebd71
asmith9455 <asmith9455>
2017-05-27 22:23:00 -0400

    Finished a simple triangle generator.

--- geometry/main.cpp
+++ geometry/main.cpp
@@ -8,5 +8,10 @@ int main()
 	TriangleGenerator<Point3, int> t(0);
 	Point3<int> p = t.test();
 
+	vector<Triangle<Point3, int>> triangles
+		= t.getTrianglesFrom2dGrid(1, 1, 10, 10);
+
+
+
 	return 0;
 }
